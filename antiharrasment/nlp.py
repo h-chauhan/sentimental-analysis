f6f29f8f2aff1775455167f0d7793b946dd55d1f
Himanshu Chauhan <hchauhan1404@outlook.com>
2018-01-08 06:48:42 +0530

    bug fix

--- antiharrasment/nlp.py
+++ antiharrasment/nlp.py
@@ -9,54 +9,54 @@ nltk.download('stopwords')
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
 
-from .models import *
-dataset = query.objects.all()
-
-corpus = []
-for query in dataset:
-    sstatement = re.sub('[^a-zA-Z]', ' ', query.statement).lower().split()
-    ps = PorterStemmer()
-    sstatement = ' '.join([ps.stem(word) for word in sstatement if not word in set(stopwords.words('english'))])
-    corpus.append(sstatement)
-
-uniqueWords = {}
-count = 0
-for line in corpus:
-    for word in line.split(' '):
-        if word not in uniqueWords.keys():
-            uniqueWords[word] = count
-            count += 1
-
-train_input_vector = []
-for line in corpus:
-    arr = np.zeros(count)
-    for word in line.split(' '):
-        arr[uniqueWords[word]] = 1
-    train_input_vector.append(arr)
-
-train_input = np.array(train_input_vector)
-
-sigmoid = lambda x: 1/(1+np.exp(-x))
-sigmoid_graph = lambda x: x*(1-x)
-
-y = [1 if x.is_harrassing else 0 for x in dataset]
-
-max_iter = 10000
-learning_rate = 0.3
-intercept = np.ones((train_input.shape[0],1))
-train_input = np.hstack((intercept, train_input))
-theta = np.zeros(train_input.shape[1])
-train_Y_arr = np.array(y)
-for iter in range(max_iter):
-	current_val = np.dot(train_input,theta)
-	current_predictions = sigmoid(current_val)
-	#Update Theta
-	error = train_Y_arr - current_predictions
-	gradient = np.dot(train_input.T, error)
-	theta += learning_rate * gradient
+from .models import query as querydbset
 
 def pred(statement):
-    global count
+    dataset = querydbset.objects.all()
+
+    corpus = []
+    for query in dataset:
+        sstatement = re.sub('[^a-zA-Z]', ' ', query.statement).lower().split()
+        ps = PorterStemmer()
+        sstatement = ' '.join([ps.stem(word) for word in sstatement if not word in set(stopwords.words('english'))])
+        corpus.append(sstatement)
+
+    uniqueWords = {}
+    count = 0
+    for line in corpus:
+        for word in line.split(' '):
+            if word not in uniqueWords.keys():
+                uniqueWords[word] = count
+                count += 1
+
+    train_input_vector = []
+    for line in corpus:
+        arr = np.zeros(count)
+        for word in line.split(' '):
+            arr[uniqueWords[word]] = 1
+        train_input_vector.append(arr)
+
+    train_input = np.array(train_input_vector)
+
+    sigmoid = lambda x: 1/(1+np.exp(-x))
+    sigmoid_graph = lambda x: x*(1-x)
+
+    y = [1 if x.is_harrassing else 0 for x in dataset]
+
+    max_iter = 10000
+    learning_rate = 0.3
+    intercept = np.ones((train_input.shape[0],1))
+    train_input = np.hstack((intercept, train_input))
+    theta = np.zeros(train_input.shape[1])
+    train_Y_arr = np.array(y)
+    for iter in range(max_iter):
+        current_val = np.dot(train_input,theta)
+        current_predictions = sigmoid(current_val)
+        #Update Theta
+        error = train_Y_arr - current_predictions
+        gradient = np.dot(train_input.T, error)
+        theta += learning_rate * gradient
+
     test_input_vector = []
     arr = np.zeros(count)
 
